18036df502b65ba5fc98772a25cc07ac918a0fe5
Ericgacoki <gacokieric@gmail.com>
2021-01-14 03:13:19 +0300

    ...

--- app/src/main/java/com/dialogsinandroid/MainActivity.kt
+++ app/src/main/java/com/dialogsinandroid/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.dialogsinandroid.databinding.ActivityMainBinding
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private var activityMainBinding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,21 +16,23 @@ class MainActivity: AppCompatActivity() {
         handleButtonClicks()
     }
 
-    private fun handleButtonClicks(){
+    private fun handleButtonClicks() {
         activityMainBinding?.basicDialog?.setOnClickListener {
             showDialog()
         }
     }
-    private fun showDialog(){
+
+    private fun showDialog() {
         val alertDialogB = AlertDialog.Builder(this)
         alertDialogB.apply {
             setTitle("Hello Dude")
-            setPositiveButton("Positive"){_,_ ->
+            setPositiveButton("Positive") { _, _ ->
             }
-            setNegativeButton("Negative"){_,_ ->
+            setNegativeButton("Negative") { _, _ ->
             }
         }.create().show()
     }
+
     /* avoid holding references to non-existing locations*/
     override fun onDestroy() {
         super.onDestroy()
